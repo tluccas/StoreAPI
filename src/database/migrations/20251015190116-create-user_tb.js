ae443864e28290298d96f29886ca1b951fbc9d4f
Lucas Alves <lucas1901alves@gmail.com>
2025-10-15 16:31:30 -0300

    feat(migrate): user_tb migrate

--- src/database/migrations/20251015190116-create-user_tb.js
+++ src/database/migrations/20251015190116-create-user_tb.js
@@ -3,20 +3,48 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    return queryInterface.createTable("users_tb", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
 
-  async down(queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      role: {
+        type: Sequelize.STRING,
+        defaultValue: "user",
+      },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+  async down(queryInterface) {
+    return queryInterface.dropTable("users_tb");
   },
 };
