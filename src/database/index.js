ac8312283a1ce37651287f3fab7c2f95ed930624
Lucas Alves <lucas1901alves@gmail.com>
2025-10-18 03:16:58 -0300

    feat(database): DataBase class

---
+++ src/database/index.js
@@ -0,0 +1,24 @@
+import Sequelize from "sequelize";
+import config from "../config/database.js";
+import User from "../app/models/entity/User.js";
+import Cart from "../app/models/entity/Cart.js";
+import Order from "../app/models/entity/Order.js";
+import OrderItem from "../app/models/entity/OrderItem.js";
+import Payment from "../app/models/entity/Payment.js";
+import Product from "../app/models/entity/Product.js";
+import Category from "../app/models/entity/Category.js";
+
+const models = [User, Cart, Order, OrderItem, Payment, Product, Category];
+
+class DataBase {
+  constructor() {
+    this.connection = new Sequelize(config);
+    this.init();
+  }
+
+  init() {
+    models.forEach((model) => model.init(this.connection));
+  }
+}
+
+export default DataBase;
