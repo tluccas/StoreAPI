30eeafbf619d3c9a529cc208b93743e5703b1f15
Lucas Alves <lucas1901alves@gmail.com>
2025-10-28 17:30:24 -0300

    refactor(Authorization): only admin can acess PUT/DELETE :: /categorias/:id - POST :: /categorias

--- src/app/routes/CategoryRoutes.js
+++ src/app/routes/CategoryRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import CategoryController from "../controllers/CategoryController.js";
-
+import { authorize } from "../middlewares/authorize.js";
 const routes = new Router();
 
 routes.get("/", CategoryController.findAll);
 routes.get("/:id", CategoryController.findById);
-routes.post("/", CategoryController.create);
-routes.put("/:id", CategoryController.update);
-routes.delete("/:id", CategoryController.delete);
+routes.post("/", authorize("admin"), CategoryController.create);
+routes.put("/:id", authorize("admin"), CategoryController.update);
+routes.delete("/:id", authorize("admin"), CategoryController.delete);
 
 export default routes;
