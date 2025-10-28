da9c28edc07a8204206ab55c8aae84f1cda64421
Lucas Alves <lucas1901alves@gmail.com>
2025-10-28 17:26:13 -0300

    refactor(Authorization): only admin can acess GET :: /payments

--- src/app/routes/PaymentRoutes.js
+++ src/app/routes/PaymentRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import PaymentController from "../controllers/PaymentController.js";
+import { authorize } from "../middlewares/authorize.js";
 
 const routes = new Router();
 
 routes.get("/order/:orderId", PaymentController.findByOrderId);
-routes.get("/", PaymentController.findAll);
+routes.get("/", authorize("admin"), PaymentController.findAll);
 routes.get("/:id", PaymentController.findById);
 routes.post("/", PaymentController.create);
 routes.put("/status/:id", PaymentController.updateStatus);
