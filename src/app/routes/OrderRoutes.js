b8f9ea510917c46f07d507ef289f3a7579745821
Lucas Alves <lucas1901alves@gmail.com>
2025-10-28 17:27:44 -0300

    refactor(Authorization): only admin can acess GET :: /pedidos

--- src/app/routes/OrderRoutes.js
+++ src/app/routes/OrderRoutes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import OrderController from "../controllers/OrderController.js";
-
+import authMiddleware from "../middlewares/auth.js";
+import { authorize } from "../middlewares/authorize.js";
 const routes = new Router();
 
-routes.get("/user/:userId", OrderController.findByUserId);
-routes.get("/", OrderController.findAll);
+routes.get("/users/:userId", OrderController.findByUserId);
+
+routes.post("/checkout", authMiddleware, OrderController.checkout);
+
+routes.get("/", authorize("admin"), OrderController.findAll);
 routes.get("/:id", OrderController.findById);
-routes.post("/", OrderController.create);
+routes.post("/", authMiddleware, OrderController.create);
 routes.put("/status/:id", OrderController.updateStatus);
 routes.delete("/:id", OrderController.delete);
 
